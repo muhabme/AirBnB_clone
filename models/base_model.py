311e96a96990ae1dd1d1443a2d4873cdb08e7912
MuhabGamal <muhabme@gmail.com>
2024-03-09 14:12:00 +0200

    base model

--- models/base_model.py
+++ models/base_model.py
@@ -2,6 +2,7 @@
 """
 Module: base_model.py
 """
+import uuid
 from datetime import datetime
 
 
@@ -10,7 +11,24 @@ class BaseModel():
     Base class defining common
     attributes/methods for other classes
     """
+    def __init__(self, *args, **kwargs):
+        """
+        Instantiates an object with all
+        of its attributes
+        """
+        if len(kwargs) > 0:
+            for key, value in kwargs.items():
+                if key == '__class__':
+                    continue
+                if key == "created_at" or key == "updated_at":
+                    value = datetime.fromisoformat(value)
+                setattr(self, key, value)
+            return
 
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+        
     def __str__(self):
         """
         Returns the string representation

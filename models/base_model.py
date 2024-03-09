912a8a3cb85eef00fc76b0182a681965cf9e9b21
MuhabGamal <muhabme@gmail.com>
2024-03-09 14:08:17 +0200

    base model

---
+++ models/base_model.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""
+Module: base_model.py
+"""
+from datetime import datetime
+
+
+class BaseModel():
+    """
+    Base class defining common
+    attributes/methods for other classes
+    """
+
+    def __str__(self):
+        """
+        Returns the string representation
+        of the instance
+        """
+        return "[{}] ({}) {}".format(
+            type(self).__name__, self.id, self.__dict__)
+
+    def save(self):
+        """
+        Updates the public instance attribute
+        updated_at with the current datetime
+        """
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        """
+        returns a dictionary containing all keys/values
+        of __dict__ of the instance
+        """
+        dict = {**self.__dict__}
+        dict['__class__'] = type(self).__name__
+        dict['created_at'] = dict['created_at'].isoformat()
+        dict['updated_at'] = dict['updated_at'].isoformat()
+
+        return dict
